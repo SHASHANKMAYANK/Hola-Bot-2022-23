acf1e2b02371f3109cf3d1f9b6ffc96819bd5f48
SHASHANK <106818829+SHASHANKMAYANK@users.noreply.github.com>
2023-12-23 20:06:08 +0530

    Add files via upload

---
+++ catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/shashank/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/shashank/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/shashank/catkin_ws/devel;/opt/ros/noetic' != '' else []
