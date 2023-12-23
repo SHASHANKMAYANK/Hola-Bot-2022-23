f4a20ac2712365b5bddcde7e924f8ea9aaee02a6
SHASHANK <106818829+SHASHANKMAYANK@users.noreply.github.com>
2023-12-23 20:23:38 +0530

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/shashank/catkin_ws/src
+# Build directory: /home/shashank/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("eYRC-2022_HolA_Bot/task_0_boilerplate/eyrc-2022_holabot")
+subdirs("hola")
+subdirs("eYRC-2022_HolA_Bot/task_2_boilerplate")
---
+++ urdf_tut/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shashank/catkin_ws/src/urdf_tut
+# Build directory: /home/shashank/catkin_ws/build/urdf_tut
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
